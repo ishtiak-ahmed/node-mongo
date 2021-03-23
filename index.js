4ad2cfcb473c907c8e98a3bb1d820ef7f81b44fe
Ishtiak Ahmed <iahmed1606@gmail.com>
2021-03-23 23:29:25 +0600

    cors installed

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
+
 app.get('/', (req, res) => {
-    res.send('I am running node server..')
+    res.send('I am running node server with nodemon..')
+})
+
+app.get('/hello', (req, res) => {
+    res.send(
+        { hello: 'Hello', world: 'World' }
+    )
+})
+
+const usersObj = [{ id: 1, name: 'Emma' }, { id: 2, name: 'Leena' }, { id: 3, name: 'Hermione' }]
+const users = ['Emma', 'Leena', 'Jane', 'Georgina', 'Hermione']
+app.get('/user/:id', (req, res) => {
+    const userId = req.params.id
+    const user = usersObj.find(({ id }) => id == userId)
+    res.send(user)
 })
 
 app.listen(3000, () => {
