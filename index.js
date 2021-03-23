dfe85d408a32a6a6f214692ec6f9aae3999e4ca1
Ishtiak Ahmed <iahmed1606@gmail.com>
2021-03-24 00:47:44 +0600

    post method added

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+var bodyParser = require('body-parser')
 
 app.use(cors())
+app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
-    res.send('I am running node server with nodemon..')
-})
 
-app.get('/hello', (req, res) => {
-    res.send(
-        { hello: 'Hello', world: 'World' }
-    )
-})
+const users = [{ id: 1, name: 'Emma' }, { id: 2, name: 'Leena' }, { id: 3, name: 'Hermione' }]
 
-const usersObj = [{ id: 1, name: 'Emma' }, { id: 2, name: 'Leena' }, { id: 3, name: 'Hermione' }]
-const users = ['Emma', 'Leena', 'Jane', 'Georgina', 'Hermione']
+
 app.get('/user/:id', (req, res) => {
     const userId = req.params.id
-    const user = usersObj.find(({ id }) => id == userId)
+    const user = users.find(({ id }) => id == userId)
     res.send(user)
+})
+
+app.post('/addUser', (req, res) => {
+    console.log(req.body)
 })
 
 app.listen(3000, () => {
